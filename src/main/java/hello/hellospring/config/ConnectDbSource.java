1f25d08325561afd1003d57da81d6df24a470d33
isun-dev <florachoi1124@naver.com>
2022-07-05 13:58:45 +0900

    feat: add docker file, build jar file, run the jar file on the docker

---
+++ src/main/java/hello/hellospring/config/ConnectDbSource.java
@@ -0,0 +1,48 @@
+package hello.hellospring.config;
+
+import java.sql.*;
+
+public class ConnectDbSource {
+    // Connect to your database.
+    // Replace server name, username, and password with your credentials
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        String connectionUrl =
+                "jdbc:sqlserver://127.0.0.1:1433;"
+                        + "database=master;"
+                        + "user=sa;"
+                        + "password=1StrongPwddocker images";
+        System.out.println("데이터 베이스 커넥스 테스트 1-1");
+        try (Connection connection = DriverManager.getConnection(connectionUrl);) {
+            System.out.println("데이터 베이스 커넥스 테스트 [커넥트 성공]");
+            // Code here.
+            Statement stmt = connection.createStatement();
+            System.out.println("데이터 베이스 커넥스 테스트 [커넥트 스트림 성공]");
+            // ResultSet rs = stmt.executeQuery("SELECT TOP 3 AddressID, AddressLine1 FROM Person.address ORDER BY AddressID ASC");
+            ResultSet rs = stmt.executeQuery("select * from category");
+            System.out.println("데이터 베이스 커넥스 테스트 [쿼리 call 성공]");
+            System.out.println(rs);
+            while (rs.next()) {
+                String categoryId = rs.getString("category_id");
+                String categoryName = rs.getString("category_name");
+                System.out.println(categoryId + ", " + categoryName);
+                System.out.println("==========");
+            }
+            //
+            //            while (rs.next()) {
+            //                int AddressID = rs.getInt("AddressID");
+            //                String AddressLine1 = rs.getString("AddressLine1");
+            //
+            //                System.out.println(AddressID + ", " + AddressLine1);
+            //            }
+            //            rs.close();
+            stmt.close();
+            connection.close();
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

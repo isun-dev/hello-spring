96fa6b22c6ba0fbe445dedc070fb7982a17ab29f
isun-dev <florachoi1124@naver.com>
2022-07-19 13:59:49 +0900

    feat: MemberService의 Test 코드 작성

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -4,7 +4,10 @@ package hello.hellospring.service;
 // 테스트코드를 작성할 때는 함수명을 한글로 작성해도 된다.
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,7 +15,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memoryMemberRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        memoryMemberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memoryMemberRepository);
+
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memoryMemberRepository.clearStore();
+    }
 
     @Test
     void 회원가입() {
@@ -24,7 +40,28 @@ class MemberServiceTest {
 
         // then -> 어떠한 결과가 나와야 한다.
         Member findMember = memberService.findOne(saveId).get();
-        // assertThat(member.getName())
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("asd");
+
+        Member member2 = new Member();
+        member2.setName("asd");
+
+        // when
+        memberService.join(member1);
+        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalStateException e) {
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+        }
+
+        // then
     }
 
     @Test

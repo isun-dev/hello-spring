40f0b8db2c7eed24b1d8c6cb79d25525218a730b
isun-dev <florachoi1124@naver.com>
2022-07-20 19:16:42 +0900

    feat: 파일 수정

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -8,9 +8,7 @@ import org.springframework.stereotype.Controller;
 public class MemberController {
 
     // 필드에 Autowired를 주입하는 것은 추후에, 필드 변경이 어려워 질 수 있기 때문에 가급적 하지 않는게 좋다.
-
-
-    private MemberService memberService;
+    private final MemberService memberService;
 
 
     // setter 주입 : 누군가가 MemberController를 클릭 했을 때 setter 주입이 열려 있어야 한다.
